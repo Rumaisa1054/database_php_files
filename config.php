dcad2315508c0a169e8363d09d8824d40fa98095
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2025-02-15 20:43:26 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,17 @@
 <?php
 
-$host = "localhost";
+$host = "database-fyp.cr4e86o88i1g.eu-north-1.rds.amazonaws.com";
+$port = 3306;
 $username = "root";
-$password = "";
-$db_name = "medhealth";
-$connection = mysqli_connect($host, $username, $password, $db_name);
+$password = "0XA4MpW6LQo8kEvqKCyH"; 
+$db_name = "database-fyp";
+
+// Corrected connection with port
+$connection = mysqli_connect($host, $username, $password, $db_name, $port);
 
 if (!$connection) {
-    # code...
-    echo "Connection Failed";
+    die("Connection Failed: " . mysqli_connect_error());
+} else {
+    echo "Connected Successfully!";
 }
+?>
