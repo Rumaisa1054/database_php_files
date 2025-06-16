8f9ef7937749c76b5c08afe7a683c67733e9f0d7
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2025-06-16 06:24:35 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,12 @@
 <?php
 
-$host = "database-fyp.cr4e86o88i1g.eu-north-1.rds.amazonaws.com";
+$host = "localhost";
 $port = 3306;
-$username = "root";
-$password = "0XA4MpW6LQo8kEvqKCyH"; 
-$db_name = "database-fyp";
+$username = "myuser";
+$password = "mypassword"; 
+$db_name = "ecom";
 
 // Corrected connection with port
 $connection = mysqli_connect($host, $username, $password, $db_name, $port);
 
-if (!$connection) {
-    die("Connection Failed: " . mysqli_connect_error());
-} else {
-    echo "Connected Successfully!";
-}
 ?>
