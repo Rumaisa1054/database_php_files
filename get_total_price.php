b415c7de4fcd13db35a89cf38294b2b139dd5bb8
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2025-06-16 06:33:05 +0500

    Update get_total_price.php

--- get_total_price.php
+++ get_total_price.php
@@ -6,7 +6,7 @@ require "config.php";
 $response = array();
 $userID = $_GET['userID'];
 
-$query_total_price = mysqli_query($connection, "SELECT SUM(quantity*price) as Total FROM cart WHERE id_user='$userID'");
+$query_total_price = mysqli_query($connection, "SELECT SUM(quantity*price) as Total FROM cart WHERE id_device='$userID'");
 
 $result = mysqli_fetch_array($query_total_price);
 
