642ae7a25ecdd42fed139032050f53a88bea8317
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2025-08-21 02:44:04 +0500

    Update total_cart.php

--- total_cart.php
+++ total_cart.php
@@ -5,12 +5,12 @@ require 'config.php';
 $response = array();
 $userID = $_GET['userID'];
 
-$query_select_cart = mysqli_query($connection, "SELECT COUNT(*) as Jumlah FROM cart WHERE id_user = '$userID'");
+$query_select_cart = mysqli_query($connection, "SELECT COUNT(*) as tot FROM cart WHERE id_user = '$userID'");
 
 while ($row = mysqli_fetch_array($query_select_cart)) {
     # code...
 
-    $key['Jumlah'] = $row['Jumlah'];
+    $key['total'] = $row['total'];
 
     array_push($response, $key);
 }
